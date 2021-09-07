bd85aa8599b933899e3f7375913e45b265d9aafa
smudesign <urwinsamantha@gmail.com>
2021-09-07 18:52:13 -0400

    added functionality to the form

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,18 @@
-// DOM object reference for the Add Task button
-var buttonEl = document.querySelector("#save-task");
+// DOM object reference for the form element with the id #task-form
+var formEl = document.querySelector("#task-form");
 // DOM object reference for the tasks to do ul
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
 // a function that will: create a li, with the class #task-item, that says "This is a new task.", finally add (append) this li to the end of the ul
-var createTaskHandler = function() {
+var createTaskHandler = function(event) {
+
+    event.preventDefault();
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
     listItemEl.textContent = "This is a new task.";
     tasksToDoEl.appendChild(listItemEl);
 };
 
-// When the button is clicked perform the createTaskHandler() function
-buttonEl.addEventListener("click", createTaskHandler);
+// When the form is submited perform the createTaskHandler() function
+formEl.addEventListener("submit", createTaskHandler);
